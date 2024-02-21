bc81fdae72db22f25dadae4c4f35a15137a1eb67
Szczepan <micek.szczepan.w@gmail.com>
2024-02-21 21:12:36 +0100

    refactor(functions): rename form -> functions.ts to math-operations.ts and enhance method readability

---
+++ examples/module1/lesson1/solver/math-operations.ts
@@ -0,0 +1,6 @@
+import type { MathOperation } from './types.ts';
+
+export const add: MathOperation = (a, b) => a + b;
+export const subtract: MathOperation = (a, b) => a - b;
+export const multiply: MathOperation = (a, b) => a * b;
+export const divide: MathOperation = (a, b) => a / b;
